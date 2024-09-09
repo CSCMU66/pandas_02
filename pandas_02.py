718a914633cbe329a28e3f6eb6934ad4f6b9e9cb
Ratsameetip Wita <ratsameetip.w@cmu.ac.th>
2024-09-09 18:17:27 +0700

    init commit

---
+++ pandas_02.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# ชื่อ นามสกุล (ชื่อเล่น)
+# 6XXXXXXXX
+# HW11_2
+# 204203 Sec 00?
+
+# # Ecommerce Purchases Exercise
+
+
+# 0) ** Import pandas
+# 0) read in the "Ecommerce.Purchases.csv" file and set it to a DataFrame called ecom. **
+
+# display the correct output in each function
+
+# 1) **Check the head of the DataFrame.**
+
+def q01():
+    print("\nQ01")
+
+
+# 2) ** How many rows and columns are there? **
+
+def q02():
+    print("\nQ02")
+
+
+# 3) ** What is the average Purchase Price? **
+
+def q03():
+    print("\nQ03")
+
+# 4) ** What were the highest and lowest purchase prices? **
+
+
+def q04():
+    print("\nQ04")
+
+
+# 5) ** How many people have English 'en' as their Language of choice on the website? **
+
+def q05():
+    print("\nQ05")
+
+
+# 6) ** How many people have the job title of "Lawyer" ? **
+#
+
+def q06():
+    print("\nQ06")
+
+
+# 7) ** How many people made the purchase during the AM and how many people made the purchase during PM ? **
+#
+# **(Hint: Check out [value_counts()](http://pandas.pydata.org/pandas-docs/stable/generated/pandas.Series.value_counts.html) ) **
+
+def q07():
+    print("\nQ07")
+
+
+# 8) ** What are the 5 most common Job Titles? **
+
+def q08():
+    print("\nQ08")
+
+
+# 9) ** Someone made a purchase that came from Lot: "90 WT" , what was the Purchase Price for this transaction? **
+
+def q09():
+    print("\nQ09")
+
+
+# 10) ** What is the email of the person with the following Credit Card Number: 4926535242672853 **
+
+def q10():
+    print("\nQ10")
+
+
+# 11) ** How many people have American Express as their Credit Card Provider *and* made a purchase above $95 ?**
+
+def q11():
+    print("\nQ11")
+
+
+# 12) ** Hard: How many people have a credit card that expires in 2025? **
+
+def q12():
+    print("\nQ12")
+
+
+# 13) ** Hard: What are the top 5 most popular email providers/hosts (e.g. gmail.com, yahoo.com, etc...) **
+
+
+def q13():
+    print("\nQ13")
+
+
+if __name__ == '__main__':
+    q01()
+    q02()
+    q03()
+    q04()
+    q05()
+    q06()
+    q07()
+    q08()
+    q09()
+    q10()
+    q11()
+    q12()
+    q13()
